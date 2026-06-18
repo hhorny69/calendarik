fa3575d35a3edb1b0120b0637b79aa4ba78da022
hhorny69 <maskarova.sabina@gmail.com>
2026-06-18 12:22:33 +0500

    update config for render

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-﻿class Config:
-    SECRET_KEY = 'super-secret-key-12345'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Sabina123!@localhost/calendar_app'
+﻿import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-key-12345')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///calendar.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
