301243e9a78534240677c995cfe17e3a0bfca1ad
hhorny69 <maskarova.sabina@gmail.com>
2026-06-18 13:36:42 +0500

    fix admin role

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
+import os
 from app import create_app, db
+from app.models import User
 
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        print('База данных создана!')
-app.run(host='0.0.0.0', port=5000, debug=False)
+        
+        # Создаём админа если его нет
+        admin = User.query.filter_by(username='admin').first()
+        if admin:
+            admin.role = 'admin'
+            db.session.commit()
+            print('Роль admin обновлена!')
+        else:
+            print('Пользователь admin не найден')
+    
+    app.run(host='0.0.0.0', port=5000, debug=False)
